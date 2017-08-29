57e4d892a7f44598119ddb9f9ef72e8fc96dee5c
Anthony St. Aubin <C-Anthony.StAubin@Charter.com>
2017-08-29 11:59:41 -0600

    Explain existing code

--- src/com/nyefan/KIA/chapter1/Example.kt
+++ src/com/nyefan/KIA/chapter1/Example.kt
@@ -8,16 +8,35 @@ package com.nyefan.KIA.chapter1
  */
 data class Person(val name: String, val age: Int? = null)
 
+/**
+ * fun main(args: Array<String>) - entry point to program; functions are first-class values
+ * val persons = listOf(...) - from kotlin.collections; included in every Kotlin program
+ */
 fun main(args: Array<String>) {
 
+    /**
+     * Person("Alice"), - default parameters needn't be set explicitly
+     * Person("Bob", 29), - but they can be
+     * Person("Carl", age = 30) - they can also be named individually
+     * Person(name = "Dave", age = 31) - but all variables after the first named one must be named
+     * Person(age = 32, name = "Earl") - named variables can be passed in any order
+     */
     val persons = listOf(
             Person("Alice"),
             Person("Bob", 29),
             Person("Carl", age = 30),
-            Person(name = "Dave", age = 31)
+            Person(name = "Dave", age = 31),
+            Person(age = 32, name = "Earl")
     )
 
+    /**
+     * val oldest: Person? = ... - value types can be explicitly declared, though they needn't be
+     * = persons.maxBy { it.age ?: 0 } - ???? something to do with lambdas; ?: is the elvis operator
+     */
     val oldest: Person? = persons.maxBy { it.age ?: 0 }
 
+    /**
+     * println("The oldest is: $oldest") - String.format works by default and automatically pulls variables into scope
+     */
     println("The oldest is: $oldest")
 }
